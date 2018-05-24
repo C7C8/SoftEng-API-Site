a7b09adf49f91a486f6c12c70f4ff3cab1ddbbbb
Sourec <tothemyers@gmail.com>
2018-05-24 17:35:30 -0400

    Add markdown HTML rendering (scary!)

--- app/services/apilist.js
+++ app/services/apilist.js
@@ -1,6 +1,17 @@
 app.factory("apilist", ["$http", function($http) {
 	return $http.get("http://localhost:8000/apilist.json")
 		.success(function(data){
+			//Render description markdown as HTML
+			let conv = new showdown.Converter();
+			conv.setOption("headerLevelStart", 5);
+			for (i in data.classes){
+				cl = data.classes[i];
+				for (j in cl.apis){
+					api = cl.apis[j];
+					api.description = conv.makeHtml(api.description.replace(/\\n/g, "\n"));
+				}
+			}
+
 			return data;
 		})
 		.error(function(err){
