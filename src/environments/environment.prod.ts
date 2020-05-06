c42b7155a2e1eff575a2dda5cc00456c18df07b5
Sourec <tothemyers@gmail.com>
2020-05-05 20:02:46 -0400

    Emergency fix all api URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,14 +2,14 @@ export const environment = {
   production: true,
   api: {
     list: '/list.json',
-    login: '/auth/login',
-    register: '/auth/register',
-    deregister: '/auth/register',
-    create: '/list',
-    update: '/list',
-    delete: '/list',
-    userlist: '/admin',
-    deleteuser: '/admin',
-    moduser: '/admin'
+    login: 'https://api.apisite.crmyers.dev/auth/login',
+    register: 'https://api.apisite.crmyers.dev/auth/register',
+    deregister: 'https://api.apisite.crmyers.dev/auth/register',
+    create: 'https://api.apisite.crmyers.dev/list',
+    update: 'https://api.apisite.crmyers.dev/list',
+    delete: 'https://api.apisite.crmyers.dev/list',
+    userlist: 'https://api.apisite.crmyers.dev/admin',
+    deleteuser: 'https://api.apisite.crmyers.dev/admin',
+    moduser: 'https://api.apisite.crmyers.dev/admin'
   }
 };
